ea0ab6434d063acdf7f4d0c649efc7bc9cfca6df
Chra Hussain <chra.hussain@gmail.com>
2020-10-21 18:43:06 +0300

    python script to square 3

---
+++ src/square.java
@@ -0,0 +1,12 @@
+public class square {
+
+    public static void main(String[] args) {
+
+        System.out.println(squareFunction(3));
+    }
+
+    public int suaqreFunction(int x) {
+        
+        return x*x;
+    }
+}
